cc1c63775c569a6b2ac8f5801568d9b74dbae669
adamchandra <adamchandra@gmail.com>
2013-07-15 14:24:03 -0400

    merged and upped version

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object IeslSbtBaseBuild extends Build {
 
   val iesl = "edu.umass.cs.iesl"
   val scalaV = "2.9.2"
-  val vers = "51"
+  val vers = "55"
 
   val packageTemplate = TaskKey[File]("package-template")
 
