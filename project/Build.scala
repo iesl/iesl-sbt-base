79780296232f08539a8ddcf8753bb3a71b0ad4df
David Soergel <dev@davidsoergel.com>
2013-07-15 11:01:17 -0400

    specs2 scala 2.10 issue, try snapshot instead of release

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object IeslSbtBaseBuild extends Build {
 
   val iesl = "edu.umass.cs.iesl"
   val scalaV = "2.10.2"
-  val vers = "51"
+  val vers = "52"
 
   val packageTemplate = TaskKey[File]("package-template")
 
