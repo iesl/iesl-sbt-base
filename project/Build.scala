91a336a87a96cf5323cfb9bb6a4129a4aca30619
David Soergel <dev@davidsoergel.com>
2013-08-26 16:27:28 -0400

    bump version (??)

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object IeslSbtBaseBuild extends Build {
 
   val iesl = "edu.umass.cs.iesl"
   val scalaV = "2.9.2"
-  val vers = "56"
+  val vers = "57"
 
   val packageTemplate = TaskKey[File]("package-template")
 
