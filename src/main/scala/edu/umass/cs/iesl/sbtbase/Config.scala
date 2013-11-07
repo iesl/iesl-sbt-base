761289ad706f7cf953ebf75649dd6471fcfa346e
David Soergel <dev@davidsoergel.com>
2013-11-07 11:44:05 -0500

    Apply Apache 2.0 License

--- src/main/scala/edu/umass/cs/iesl/sbtbase/Config.scala
+++ src/main/scala/edu/umass/cs/iesl/sbtbase/Config.scala
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2013  University of Massachusetts Amherst
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package edu.umass.cs.iesl.sbtbase
 
 import sbt._
